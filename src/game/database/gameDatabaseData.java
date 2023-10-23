5aa8ba63d1584b010a56bc6b4afde0cb3ebff621
wangs <wangs@172.29.73.13>
2023-10-23 23:35:47 +1300

    First Commit

---
+++ src/game/database/gameDatabaseData.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game.database;
+
+/**
+ *
+ * @author wangs
+ */
+public class gameDatabaseData {
+    boolean loginFlag = false;
+    boolean quitFlag = false;
+}
