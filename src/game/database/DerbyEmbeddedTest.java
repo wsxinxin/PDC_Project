f085f8843f8aa3dbdcb2bffc94ac529258aaf775
wangs <wangs@172.29.73.13>
2023-10-26 10:20:48 +1300

    First Commit

---
+++ src/game/database/DerbyEmbeddedTest.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+/**
+ *
+ * @author wangs
+ */
+public class DerbyEmbeddedTest {
+    public static void main(String[] args) {
+        String databaseURL = "jdbc:derby:C:/Users/wangs/OneDrive/Desktop/UniStuff/NetBeans Stuff/P6_18045290_21139803/PDC_Project/DB/gamedb";
+        try{
+            Connection connection = DriverManager.getConnection(databaseURL);
+            System.out.println("Connected to the Database");
+            
+            String sql = "INSERT INTO inventory (itemsinbag, weapon, potion) VALUES"
+                    + "('true', Rusty Sword', 2)";
+            Statement statement = connection.createStatement();
+            int rows = statement.executeUpdate(sql);
+            
+            if (rows > 0){
+                System.out.println("A new save state has been created");
+            }
+            
+            String shutdownURL = "jdbc:derby:;shutdown=true";
+            DriverManager.getConnection(shutdownURL);
+            
+        } catch (SQLException ex){
+            ex.printStackTrace();
+        }
+    }
+}
